b43b22a2ed62375a09cbbb5ed8297ef0f054b3fb
miguellealw <miguellealw@gmail.com>
2018-01-22 17:07:20 -0600

    added error handlers

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ const app = express();
 middleware(app);
 apiRoutes(app);
 
+// Error Handlers if other routes aren't found
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler if error is thrown from anywhere 
+// in the server by next(error)
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: error.message,
+  });
+});
+
 app.listen(keys.PORT, () => {
   console.log(`
     Server Running on Port ${keys.PORT}
