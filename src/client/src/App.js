ec211fc974c813b7264a811106bf36521bab6d83
Miguel <miguellealwebdev@gmail.com>
2018-01-26 20:09:15 -0600

    wrapper styled container around app and added global styles

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import styled, { injectGlobal } from "styled-components";
 
 /* Components */
 import Nav from "./components/Nav";
@@ -7,14 +8,32 @@ import DeckList from "./components/DeckList";
 import CardList from "./components/CardList";
 import Home from "./components/Home";
 
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+const Container = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  // background: #ccc;
+`;
+
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/deck" component={DeckList} />
-        <Route exact path="/deck/:deckName" component={CardList} />
+        <Container>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/deck" component={DeckList} />
+          <Route exact path="/deck/:deckName" component={CardList} />
+        </Container>
       </div>
     );
   }
