f4a41b62ceac2765cadd99892e61cd49fb661185
Miguel <miguellealwebdev@gmail.com>
2018-03-07 19:33:02 -0600

    Create transition component (SlideFromBottom) using react-transition-group

---
+++ src/client/src/components/animations/SlideFromBottom.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Transition } from "react-transition-group";
+
+const duration = 300;
+
+const defaultStyles = {
+  transition: `all ${duration}ms ease-in-out`,
+  transform: "translateY(-2rem)",
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { transform: "translateY(2rem)", opacity: 0 },
+  entered: { transform: "translateY(0rem)", opacity: 1 },
+
+  exiting: { transform: "translateY(0rem)", opacity: 1 },
+  exited: { transform: "translateY(2rem)", opacity: 0 },
+};
+
+const style = state => ({
+  ...defaultStyles,
+  ...transitionStyles[state],
+});
+
+const SlideFromBottom = ({ in: inProp, children }) => (
+  <Transition in={inProp} timeout={duration}>
+    {state => {
+      return children(style(state));
+    }}
+  </Transition>
+);
+
+/*
+  The Problem
+  ----
+  can't wrap children in div with styles because 
+  it overrides childrens compoents styles and causes problems
+
+  Possible Solution
+  ----
+  pass style down to children to put straint on children container
+  and not make a new one with a div
+*/
+
+export default SlideFromBottom;
