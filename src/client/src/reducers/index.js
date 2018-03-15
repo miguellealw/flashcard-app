1a43821f2723f4567a5eaa8ef7028b240789efd1
Miguel <miguellealwebdev@gmail.com>
2018-03-15 01:20:11 -0500

    Update import routes to new reducer routes and add flashReducer to root reducer

--- src/client/src/reducers/index.js
+++ src/client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import deckReducer from "./deckReducer";
+import authReducer from "./auth/authReducer";
+import deckReducer from "./decks/deckReducer";
 import errorReducer from "./errorReducer";
+import flashReducer from "./flash/flashReducer";
 
 export default combineReducers({
+  flash: flashReducer,
   errors: errorReducer,
   auth: authReducer,
   decks: deckReducer,
